d6a4b280c1fda404f7ddea8d1957ef0d49d7d8e6
Kipruto Barno <faithjebichy@gmail.com>
2024-02-25 04:10:05 +0300

    test: unit tests
    - carry out unit tests with JUnit 5

---
+++ src/test/java/com/bookshop/catalogservice/services/BookValidationTests.java
@@ -0,0 +1,48 @@
+package com.bookshop.catalogservice.services;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.bookshop.catalogservice.models.Book;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookValidationTests {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+
+        var book = new Book("1234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book("a234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations)
+                .hasSize(1);
+        assertThat(violations
+                .iterator()
+                .next()
+                .getMessage())
+                .isEqualTo("The ISBN format must be valid.");
+    }
+
+}
